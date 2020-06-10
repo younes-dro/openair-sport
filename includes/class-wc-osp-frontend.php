cc71b853e86203fc3a450e9c1a82bb69e3516961
younes-dro <dro66_8@hotmail.fr>
2020-06-10 19:20:01 +0100

    Add short title to all pages except the single product page

--- includes/class-wc-osp-frontend.php
+++ includes/class-wc-osp-frontend.php
@@ -23,11 +23,36 @@ class WC_OSP_Frontend {
     function __construct() {
         
 //        add_action('init',  array( $this , 'shorter_title' ));
-        add_filter( 'woocommerce_cart_item_name', array ( $this ,  'osp_shorter_title' ), 10, 3 );
+        add_filter( 'woocommerce_cart_item_name', array ( $this ,  'osp_shorter_title_cart' ), 10, 3 );
+        add_filter( 'the_title', array ( $this ,  'osp_shorter_title_home' ), 10, 3 );
         add_filter('woocommerce_sale_flash',  array( $this, 'osp_hide_sale_flash' ) );
         //add_action( 'wp_enqueue_scripts', array ( $this , 'frontend_scripts' ) );
     }
     
+    /**
+     * Display the product titles shorter in all store except the single product page.
+     * 
+     * @param type $title
+     * @param type $id
+     * @return string
+     */
+    function osp_shorter_title_home($title , $id ){
+        $the_product = wc_get_product( $id );
+        
+        // If  we don't have the context Product , or we  we stop here .
+        if( ! $the_product ||  is_product()  ){
+            return $title ;
+        }
+        
+        // It s a product        
+        $length_title = strlen( trim ( $title )) ;
+        $short_title = $title;
+        
+        if( $length_title >= 40 ) {
+            $short_title = substr( $short_title, 0 , 40 ) . ' ...';            
+        }
+        return $short_title;
+    }
     /**
      * Display the product titles shorter in Cart and Checkout pages.
      * 
@@ -36,7 +61,7 @@ class WC_OSP_Frontend {
      * @param type $cart_item_key
      * @return type String
      */
-    public function osp_shorter_title( $item_name,  $cart_item,  $cart_item_key ) {
+    public function osp_shorter_title_cart( $item_name,  $cart_item,  $cart_item_key ) {
         
         $length_title = strlen( trim( get_the_title ( $cart_item['product_id'] ) ) );
         $short_title = get_the_title ( $cart_item['product_id'] );
